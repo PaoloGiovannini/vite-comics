4989f36a2b58c514591d6112e59a86913a4bb4b7
Paolo Giovannini <giovannini13@gmail.com>
2023-03-27 17:00:25 +0200

    esercizio concluso con bonus

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -7,8 +7,35 @@
     
 <template>
   <main>
-    <div class="wrap">
+    <div class="wrap-up">
       <div class="container">aggiungere contenuto</div>
+    </div>
+    <div>
+      <div class="wrap-down">
+        <div class="container d-flex space-around">
+          <div class="d-flex flex-center">
+            <img src="../assets/buy-comics-digital-comics.png" alt="digital comics">
+            <span>DIGITAL COMICS</span>
+          </div>
+          <div class="d-flex flex-center">
+            <img src="../assets/buy-comics-merchandise.png" alt="Merchandise">
+            <span>DC MERCHANDISE</span>
+          </div>
+          <div class="d-flex flex-center">
+            <img src="../assets/buy-comics-subscriptions.png" alt="subscription">
+            <span>SUBSCRIPTION</span>
+          </div>
+          <div class="d-flex flex-center">
+            <img src="../assets/buy-comics-shop-locator.png" alt="Comic shop locator">
+            <span>COMIC SHOP LOCATOR</span>
+          </div>
+          <div class="d-flex flex-center">
+            <img src="../assets/buy-dc-power-visa.svg" alt="DC power visa">
+            <span>DC POWER VISA</span>
+          </div>
+        </div>
+      </div>
+      
     </div>
   </main> 
     
@@ -16,10 +43,31 @@
     
 <style scoped lang="scss">
 @use"../styles/partials/variables" as *;
-.wrap{
+.wrap-up{
   background-color: $bg-main;
   color: $text-main;
-  padding: 40px 0px;
+  padding: 60px 0px;
 }
+
+.wrap-down{
+  background-color: $color-hover;
+
+  .container{
+    padding: 40px 0px;
+
+    img{
+      display: inline-block;
+      width: 50px;
+      vertical-align: middle;
+    }
+    span{
+      vertical-align: middle ;
+      color: $text-main;
+    }
+  }
+
+  
+}
+
    
 </style>
